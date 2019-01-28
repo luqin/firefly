bc33894e2cea6f18c7dceee37e63efc3b0a6014d
bqw_5189 <bqw_5189@163.com>
2019-01-28 10:39:50 +0800

    abupy

--- abuui/settings.py
+++ abuui/settings.py
@@ -78,13 +78,21 @@ TEMPLATES = [
 WSGI_APPLICATION = 'abuui.wsgi.application'
 
 
-# Database
+# Database CREATE DATABASE IF NOT EXISTS abuui DEFAULT CHARSET utf8 COLLATE utf8_general_ci;
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    # }
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'abuui',
+        'USER': 'root',
+        'PASSWORD': 'root',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
     }
 }
 
