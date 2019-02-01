fa0e7dd826398c5bc078a1aae4c587f1ded6b3f6
x <luqinx@qq.com>
2019-02-01 21:11:13 +0800

    kline

--- apps/charts/urls.py
+++ apps/charts/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 
 from . import views
+from .views_k import k
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('k/', views.k, name='k'),
+    path('k/', k.index, name='k'),
+    path('k1/', views.k, name='k1'),
 ]
